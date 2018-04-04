511af79fa442a8381d7496c9e2ecd332feda1f1e
Anoop Sharma <anoopshar@cadomain.local>
2018-04-05 02:34:00 +0530

    Setup Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;\
+mongoose.Promise = global.Promise;
 
 var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp';
 mongoose.connect(url);
